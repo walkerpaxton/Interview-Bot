588c606c32ee696976ed8343c325c5defbb313ed
walkerpaxton <walkpax@gmail.com>
2025-07-01 10:09:08 +0200

    Basic next.js set up

---
+++ frontend/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div id = "website">
+      <header>"CS Interview Prepper"</header>
+      {
+        
+      }
+    </div>
+  );
+} 
