435113e7d9df498efa4a4b3055dbcdec1f64d65e
walkerpaxton <walkpax@gmail.com>
2025-07-03 11:04:22 +0200

    text box and submission button added

--- frontend/pages/interviewPage.js
+++ frontend/pages/interviewPage.js
@@ -1,14 +1,54 @@
 import React, { useState } from 'react';
 
 export default function Home() {
+    const [postContent, setPostContent] = useState('');
+    const submitText = () => {
+
+    }
     return (
-    <div id = "title">
-        <h1 style = {{
-            textAlign: "center",
-            fontSize: "4rem"
-        }}>
+    <div id = "webpage">
+
+        <div id = "title">
+            <h1 style = {{
+                textAlign: "center",
+                fontSize: "4rem"
+            }}>
             Interview Bot Page, Implement Later
-        </h1>
+            </h1>
+        </div>
+
+        <div id = "text">
+            <textarea 
+            style = {{
+                position: "fixed",
+                left: "50%",
+                bottom: "100px",
+                transform: "translateX(-50%)",
+                borderRadius: "20px",
+                padding: "10px",
+                width: "500px",
+                height: "50px",
+                borderColor: "black",
+                fontSize: "20px"
+            }}
+            value = {postContent}
+            onChange={e => setPostContent(e.target.value)}
+            />
+            <button
+            type = "submit"
+            onClick = {() => submitText()}
+            style = {{
+                position: "absolute",
+                borderColor: "grey",
+                left: "64%", 
+                bottom: "105px", 
+                borderRadius: "20px",
+                backgroundColor: "black",
+                color: "white"
+            }}>
+                Submit
+            </button>
+        </div>
     </div>
     );
 }
