d70a42469f0f669feaf5bffa40e0f20ff33f735e
walkerpaxton <walkpax@gmail.com>
2025-07-10 14:20:38 +0200

    Made it so that study prep page can read in from a file. We should probably make it so that an ai goes through and formats all the data. Or we could change the data to just be proofs.

--- frontend/pages/studyPrepPage.js
+++ frontend/pages/studyPrepPage.js
@@ -1,15 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Home() {
+    const [slides, setSlides] = useState([]);
+
+    useEffect(() => {
+        fetch('/slides.json')
+        .then(res => res.json())
+        .then(data => setSlides(data))
+    }, []);
+
     return (
-    <div id = "title">
-        <h1 style = {{
-            textAlign: "center",
-            fontSize: "4rem",
+    <div id = "webpage">
+        <div id = "title">
+            <h1 style = {{
+                textAlign: "center",
+                fontSize: "4rem",
+                color: "white"
+            }}>
+                Study Materials Page
+            </h1>
+        </div>
+        <div id = "information"
+        style ={{
+            border: "2px solid white",
+            borderRadius: "12px",
+            maxWidth: "100%",
+            margin: "20px auto",
             color: "white"
         }}>
-            Study Materials Page, Implement Later
-        </h1>
+            {slides.map((slide, idx) =>
+            <div key={idx}>
+                {slide.file && <h2>{slide.file}</h2>}
+                <div style = {{ whiteSpace: "pre-wrap"}}>{slide.content}</div>
+                </div>
+            )}
+        </div>
     </div>
     );
 }
